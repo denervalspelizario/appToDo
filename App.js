3331bbde573ff02a542abf8d39fb1919edb9e798
denervalspelizario <denervalspelizario@gmail.com>
2023-03-08 07:16:08 -0300

    update appToDo criacao state user que renderiza screen login ou lista de tarefas de acordo com o state

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import Login from './src/components/Login'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+  const [user, setUser] = useState(null) // state user usada para controlar o login e logout se inicia null
+
+  if( !user){ // se user tiver algum dado ou seja está logado retorna componente Login
+
+    return <Login/>
+
+  } else {
+   
+    return (
+      <SafeAreaView style={styles.container}>
+  
+        <Text>Tela de Tarefas</Text>
+  
+      </SafeAreaView>
+    );
+  }
+
+
+  }
+
+
+ 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 25,
+    paddingHorizontal: 10,
+    backgroundColor: '#FFF'
   },
 });
---
+++ src/components/Login/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, TextInput  } from 'react-native';
+
+export default function Login() {
+
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+ 
+  },
+});
